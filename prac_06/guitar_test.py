6cffc4c0d25d4a7862d1b5e6387cefba3d5c2056
alikramaliziik <akram.kasozi@my.jcu.edu.au>
2026-07-08 11:23:40 +0300

    Adding code to test class methods

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 Practical - Guitar class testing
+Estimated time: 8 minutes
+Actual time: 9 minutes
+"""
+
+from guitar import Guitar
+
+
+def main():
+    """Test Guitar class methods."""
+    guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar2 = Guitar("Another Guitar", 2013, 1512.90)
+
+    print(f"{guitar1.name} get_age() - Expected 100. Got {guitar1.get_age()}")
+    print(f"{guitar2.name} get_age() - Expected 12. Got {guitar2.get_age()}")
+    print(f"{guitar1.name} is_vintage() - Expected True. Got {guitar1.is_vintage()}")
+    print(f"{guitar2.name} is_vintage() - Expected False. Got {guitar2.is_vintage()}")
+
+
+main()
